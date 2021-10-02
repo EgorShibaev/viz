490960499a949427515b1c32e67129b298f4e9e5
EgorShibaev <mister.egor03@gmail.com>
2021-10-02 22:41:27 +0300

    Update bar chart

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,30 +5,32 @@ import org.jetbrains.skija.*
 import org.jetbrains.skiko.SkiaLayer
 import org.jetbrains.skiko.SkiaRenderer
 import org.jetbrains.skiko.SkiaWindow
+import org.jetbrains.skiko.orderEmojiAndSymbolsPopup
 import java.awt.Dimension
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionAdapter
-import kotlin.math.cos
 import javax.swing.WindowConstants
-import kotlin.math.ln
-import kotlin.math.min
-import kotlin.math.sin
+import kotlin.math.*
 import kotlin.random.Random
 
 fun main() {
 	val content = listOf(
-		Pair(0.4F, "text1"), Pair(0.2F, "text2"), Pair(0.1F, "text3")
+		Pair(100f, "text1"), Pair(64f, "text2"), Pair(127f, "text3"), Pair(1000f, "text4")
 	)
-	createWindow("pf-2021-viz", Diagram.BAR_CHART, content)
-	createWindow("pf-2021-viz", Diagram.CIRCLE, content)
+	val intContent = listOf(
+		Pair(1f, 1f), Pair(0f, 2f), Pair(3f, 5f)
+	)
+	createWindow("plot", Diagram.PLOT, intContent)
+//	createWindow("bar chart", Diagram.BAR_CHART, content)
+//	createWindow("circle", Diagram.CIRCLE, content)
 
 }
 
 enum class Diagram {
-	CIRCLE, BAR_CHART
+	CIRCLE, BAR_CHART, PLOT
 }
 
-fun createWindow(title: String, type: Diagram, content: List<Pair<Float, String>>) = runBlocking(Dispatchers.Swing) {
+fun createWindow(title: String, type: Diagram, content: List<Pair<Float, Any>>) = runBlocking(Dispatchers.Swing) {
 	val window = SkiaWindow()
 	window.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
 	window.title = title
@@ -46,11 +48,10 @@ fun createWindow(title: String, type: Diagram, content: List<Pair<Float, String>
 class Renderer(
 	private val layer: SkiaLayer,
 	private val type: Diagram,
-	private val content: List<Pair<Float, String>>
+	private val content: List<Pair<Float, Any>>
 ) : SkiaRenderer {
 	private val typeface = Typeface.makeFromFile("fonts/JetBrainsMono-Regular.ttf")
 	private val font = Font(typeface, 15f)
-	private val bigFont = Font(typeface, 20f)
 	private val paint = Paint().apply {
 		color = 0xff000000.toInt()
 		mode = PaintMode.FILL
@@ -63,8 +64,14 @@ class Renderer(
 		strokeWidth = 2f
 	}
 
+	private val thinStroke = Paint().apply {
+		color = 0x5F000000
+		mode = PaintMode.STROKE
+		strokeWidth = 1f
+	}
+
 	private fun randomColor(seed: Float) = Paint().apply {
-		color = 0xFF000000.toInt() + Random((ln(seed) * 1000).toInt()).nextInt() % 0x1000000
+		color = 0xFF000000.toInt() + Random((ln(seed + 1) * 1e7).toInt()).nextInt() % 0x1000000
 	}
 
 	override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
@@ -73,11 +80,35 @@ class Renderer(
 		val w = (width / contentScale).toInt()
 		val h = (height / contentScale).toInt()
 
+		assert(content.isNotEmpty())
 		when (type) {
-			Diagram.BAR_CHART ->
-				barChart(canvas, w / 100F, h / 100F, w.toFloat() - 10F, h.toFloat() - 10F, content)
-			Diagram.CIRCLE ->
-				separatedCircle(canvas, w / 2F, h / 2F, min(w / 2F, h / 2F) - 30F, content)
+			Diagram.BAR_CHART -> {
+				content.forEach {
+					assert(it.second is String)
+				}
+				barChart(
+					canvas, w / 100F, h / 100F, w.toFloat() - 10F, h.toFloat() - 10F,
+					content.map { Pair(it.first, it.second as String) }
+				)
+			}
+			Diagram.CIRCLE -> {
+				content.forEach {
+					assert(it.second is String)
+				}
+				separatedCircle(
+					canvas, w / 2F, h / 2F, min(w / 2F, h / 2F) - 30F,
+					content.map { Pair(it.first, it.second as String) }
+				)
+			}
+			Diagram.PLOT -> {
+				content.forEach {
+					assert(it.second is Int)
+				}
+				plot(
+					canvas, w / 100f, h / 100f, w.toFloat() - 10F, h.toFloat() - 10F,
+					content.map { Pair(it.first, it.second as Float) }
+				)
+			}
 		}
 		layer.needRedraw()
 	}
@@ -85,19 +116,18 @@ class Renderer(
 	private fun separatedCircle(
 		canvas: Canvas, centerX: Float, centerY: Float, r: Float, content: List<Pair<Float, String>>
 	) {
-		assert(content.sumOf { it.first.toDouble() } <= 2F * Math.PI.toFloat())
-		val otherAngle = 2F * Math.PI.toFloat() - content.sumOf { it.first.toDouble() }.toFloat()
-		val fullContent = content + Pair(otherAngle, "other")
+		assert(content.all { it.first >= 0 })
+		val sum = content.sumOf { it.first.toDouble() }.toFloat()
 		var angle = 0F
 		lineInCircle(canvas, centerX, centerY, r, angle)
-		fullContent.forEach {
-			val sweepAngle = it.first
-			val text = "${it.second} - ${(100 * sweepAngle / Math.PI / 2F).toInt()}%"
+		content.forEach {
+			val sweepAngle = it.first / sum * Math.PI.toFloat() * 2
+			val text = "${it.second} - ${it.first} (${(it.first / sum * 100).toInt()}%)"
 			val prevAngle = angle
 			angle += sweepAngle
 			canvas.drawArc(
 				centerX - r, centerY - r, centerX + r, centerY + r,
-				toDegree(prevAngle) - 90F, toDegree(sweepAngle), true, randomColor(sweepAngle)
+				toDegree(prevAngle) - 90F, toDegree(sweepAngle), true, randomColor(it.first)
 			)
 			val angleForText = (prevAngle + angle) / 2
 			val yForText = centerY - (r + 15F) * cos(angleForText)
@@ -109,8 +139,8 @@ class Renderer(
 		}
 		canvas.drawCircle(centerX, centerY, r, stroke)
 		angle = 0F
-		fullContent.forEach {
-			angle += it.first
+		content.forEach {
+			angle += it.first / sum * Math.PI.toFloat() * 2
 			lineInCircle(canvas, centerX, centerY, r, angle)
 		}
 	}
@@ -129,34 +159,91 @@ class Renderer(
 	private fun barChart(
 		canvas: Canvas, left: Float, top: Float, right: Float, bottom: Float, content: List<Pair<Float, String>>
 	) {
-		assert(content.sumOf { it.first.toDouble() } <= 1F)
-		val otherAngle = 1F - content.sumOf { it.first.toDouble() }.toFloat()
-		val fullContent = content + Pair(otherAngle, "other")
+		assert(content.all { it.first >= 0 })
+		val max = content.maxOf { it.first }
+		val leftIndent = 80f
 		// widthOfColumn * cnt + widthOfColumn / 2 * (cnt - 1) = width
-		val widthOfColumn = (right - left) / (1.5F * fullContent.size - 0.5F)
-		val forText = fullContent.size * bigFont.size + 10F
-		val height = (bottom - font.size - 5F - (top + forText)) / fullContent.maxOf { it.first }
+		val widthOfColumn = (right - (left + leftIndent)) / (1.5F * content.size - 0.5F)
+		val factor = (bottom - font.size - 5F - (top + font.size + 5F)) / max
+		verticalLines(canvas, max, left, right, bottom, factor)
+		drawColumns(canvas, left + leftIndent, bottom, widthOfColumn, factor, content)
+	}
+
+	private fun verticalLines(
+		canvas: Canvas, max: Float, left: Float, right: Float, bottom: Float, factor: Float
+	) {
+		val step = max / 10f
+		(0..10).forEach {
+			canvas.drawLine(
+				left,
+				bottom - font.size - 5F - it * step * factor,
+				right,
+				bottom - font.size - 5F - it * step * factor,
+				thinStroke
+			)
+			canvas.drawString("%.2f".format(it * step), left, bottom - font.size - 8F - it * step * factor, font, paint)
+
+		}
+	}
+
+	private fun drawColumns(
+		canvas: Canvas,
+		left: Float,
+		bottom: Float,
+		widthOfColumn: Float,
+		factor: Float,
+		content: List<Pair<Float, String>>
+	) {
 		var currX = left
-		fullContent.withIndex().forEach {
-			val value = it.value.first
-			val index = it.index
+		content.forEach {
+			val value = it.first
+			val text = it.second
 			val rect = Rect(
 				currX,
-				bottom - font.size - 5F - height * value,
+				bottom - font.size - 5F - factor * value,
 				currX + widthOfColumn,
 				bottom - font.size - 5F
 			)
 			canvas.drawRect(rect, randomColor(value))
 			canvas.drawRect(rect, stroke)
-			canvas.drawString("${index + 1}", currX + widthOfColumn / 2, bottom - 2F, font, paint)
+			canvas.drawString(
+				text, currX + widthOfColumn / 2 - text.length * font.size * 0.6F / 2F, bottom - 2F, font, paint
+			)
+			if (checkInRect(rect))
+				canvas.drawString(value.toString(), State.mouseX, State.mouseY, font, paint)
 			currX += 1.5F * widthOfColumn
 		}
-		fullContent.withIndex().forEach {
-			val index = it.index + 1
-			val text = it.value.second
-			val percent = (it.value.first * 100F).toInt()
-			canvas.drawString("$index. $text - ${percent}%", left + 3F, top + index * bigFont.size, bigFont, paint)
-		}
+	}
+
+	private fun checkInRect(rect: Rect) = rect.run {
+		State.mouseX in left..right && State.mouseY in top..bottom
+	}
+
+	private fun plot(
+		canvas: Canvas,
+		left: Float,
+		top: Float,
+		right: Float,
+		bottom: Float,
+		content: List<Pair<Float, Float>>
+	) {
+		drawArrow(canvas, left, bottom, left, top, paint)
+		drawArrow(canvas, left, bottom, right, bottom, paint)
+	}
+
+	private fun drawArrow(canvas: Canvas, x0: Float, y0: Float, x1: Float, y1: Float, paint: Paint) {
+		canvas.drawLine(x0, y0, x1, y1, paint)
+		val len = 15f
+		val angle = Math.PI.toFloat() / 6
+		val vectorX = x1 - x0
+		val vectorY = y1 - y0
+		val vectorLen = sqrt(vectorX.pow(2) + vectorY.pow(2))
+		val rotatedVectorX = (vectorX * cos(angle) - vectorY * sin(angle)) * len / vectorLen
+		val rotatedVectorY = (vectorX * sin(angle) + vectorY * cos(angle)) * len / vectorLen
+		canvas.drawLine(x1 - rotatedVectorX, y1 - rotatedVectorY, x1, y1, paint)
+		val rotatedVectorX2 = (vectorX * cos(-	angle) - vectorY * sin(-angle)) * len / vectorLen
+		val rotatedVectorY2 = (vectorX * sin(-angle) + vectorY * cos(-angle)) * len / vectorLen
+		canvas.drawLine(x1 - rotatedVectorX2, y1 - rotatedVectorY2, x1, y1, paint)
 	}
 }
 
