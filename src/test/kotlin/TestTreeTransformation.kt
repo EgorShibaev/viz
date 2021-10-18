44567d7e9abfce7e6be9b6e937857191455a0c18
EgorShibaev <mister.egor03@gmail.com>
2021-10-18 21:49:08 +0300

    Add random tree test

--- src/test/kotlin/TestTreeTransformation.kt
+++ src/test/kotlin/TestTreeTransformation.kt
@@ -1,3 +1,4 @@
+import kotlin.random.Random
 import kotlin.test.*
 
 class TestTreeTransformation {
@@ -51,5 +52,52 @@ class TestTreeTransformation {
 		val path34 = TreeCell(listOf(node4), "3")
 		val path12 = TreeCell(listOf(TreeCell(listOf(path34), "2")), "1")
 		assertEquals(path12, transformTreeCells(listOf(node1, node2, node3, node4))!![0])
+	}
+
+	@Test
+	fun testUnknownChild() {
+		val node1 = TreeCell(listOf(TreeCell(emptyList(), "unknown name")), "1")
+		val node2 = TreeCell(emptyList(), "2")
+		assertEquals(null, transformTreeCells(listOf(node1, node2)))
+	}
+
+	@Test
+	fun testTwoPath() {
+		val node3 = TreeCell(emptyList(), "3")
+		val node2 = TreeCell(listOf(node3), "2")
+		val node1 = TreeCell(listOf(node2, node3), "1")
+		assertEquals(null, transformTreeCells(listOf(node1, node2, node3)))
+	}
+
+	@Test
+	fun testTwoParent() {
+		val node3 = TreeCell(emptyList(), "3")
+		val node2 = TreeCell(listOf(node3), "2")
+		val node1 = TreeCell(listOf(node3), "1")
+		assertEquals(null, transformTreeCells(listOf(node1, node2, node3)))
+	}
+
+	private fun generateRandomTree(depth: Int): TreeCell {
+		val name = "name${Random.nextInt()}"
+		if (depth == 1)
+			return TreeCell(emptyList(), name)
+		val childrenCount = Random.nextInt(1, 4)
+		val children = (1..childrenCount).map { generateRandomTree(depth - 1) }
+		return TreeCell(children, name)
+	}
+
+	private fun getListOfNodes(root: TreeCell): List<TreeCell> {
+		val rootNode = TreeCell(root.children.map { TreeCell(emptyList(), it.name) }, root.name)
+		return listOf(rootNode) + root.children.map { getListOfNodes(it) }.flatten()
+	}
+
+	@Test
+	fun testRandomTree() {
+		val testCount = 10
+		repeat(testCount) {
+			val randomTree = generateRandomTree(3)
+			val listOfNodes = getListOfNodes(randomTree)
+			assertEquals(randomTree, transformTreeCells(listOfNodes)!![0])
+		}
 	}
 }
